dc8ec71f426d9f31c9718146cd7808335a855e44
Specially4 <kotenkovsn@gmail.com>
2022-10-26 16:22:54 +0700

    create

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,35 @@
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
+
+from skymarket.users.models import User
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=100)
+    price = models.PositiveIntegerField(validators=[MinValueValidator(0)], default=0)
+    description = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    @property
+    def email(self):
+        return self.author.email if self.author else None
--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,16 +1,33 @@
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
+from django.core.validators import MinValueValidator
 from django.db import models
-from users.managers import UserManager
+from managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
 
 class UserRoles:
+    USER = 'user'
+    ADMIN = 'admin'
+    choices = (
+        (USER, 'Пользователь'),
+        (ADMIN, 'Администратор')
+    )
     # TODO закончите enum-класс для пользователя
     pass
 
 
 class User(AbstractBaseUser):
-    # TODO переопределение пользователя.
-    # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+    first_name = models.CharField(max_length=15, null=True, blank=True)
+    last_name = models.CharField(max_length=25, null=True, blank=True)
+    phone = models.CharField(max_length=12, validators=[MinValueValidator(limit_value=11, message='Invalid phone number')])
+    email = models.EmailField(unique=True)
+    role = models.CharField(choices=UserRoles.choices, default='user', max_length=13)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
